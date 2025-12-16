ce4d0c2900fa0b3f2ac6ab816e291c71d1c46c7e
andreshisas-cpu <andreshisas@gmail.com>
2025-12-16 13:50:17 -0500

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,1 @@
-import React from 'react';
-
-import { NotificationProvider } from './src/context/AddItemNotificationProvider';
-
-export const wrapRootElement = ({ element }) => (
-  <NotificationProvider>{element}</NotificationProvider>
-);
+import "./src/styles/global.css";
