4dc73b353e41a4dd62d9c0b60645ff3a5682de63
andreshisas-cpu <andreshisas@gmail.com>
2025-12-16 13:49:42 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,32 @@
+require("dotenv").config();
+
+function readServiceAccountCredentials() {
+  if (!process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS) return null;
+
+  const creds = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS);
+
+  // En Netlify suele venir con \n escapados
+  if (creds.private_key) {
+    creds.private_key = creds.private_key.replace(/\\n/g, "\n");
+  }
+  return creds;
+}
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Sydney Ecommerce Theme`,
-    siteUrl: `https://jamm.matter.design`,
+    title: "María Lucía",
+    siteUrl: "https://maria-lucia.com"
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-source-google-spreadsheet",
+      options: {
+        spreadsheetId: process.env.SPREADSHEET_ID,
+        spreadsheetName: "SiteContent",
+        typePrefix: "GoogleSheet",
+        credentials: readServiceAccountCredentials()
+      }
+    },
+    "gatsby-plugin-netlify"
+  ]
 };
